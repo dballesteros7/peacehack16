f26244a41776e1a948ef49836ca1a844aa202c95
Diego Alfonso Ballesteros Villamizar <diegob@student.ethz.ch>
2016-09-10 10:34:33 +0200

    Map start

--- burundi-app/src/App.js
+++ burundi-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BurundiMap from './BurundiMap';
 
 class App extends Component {
   render() {
@@ -8,11 +9,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React Changed</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/*<InfoBar />*/}
+        <BurundiMap />
       </div>
     );
   }
---
+++ burundi-app/src/BurundiMap.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BurundiMap extends Component {
+  render() {
+    return (
+        <div>
+          I'm a map, for real.
+        </div>
+    );
+  }
+}
+
+export default BurundiMap;
