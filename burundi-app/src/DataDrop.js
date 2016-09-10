f48abc9424d6c635fc3742a4a9caf9bb77bc37f3
syzer3@gmail.com <syzer3@gmail.com>
2016-09-10 11:13:57 +0200

    dtag drop points

--- burundi-app/src/DataDrop.js
+++ burundi-app/src/DataDrop.js
@@ -3,8 +3,28 @@ import Dropzone from 'react-dropzone'
 
 class DataDrop extends Component {
 
+    constructor(props) {
+        super(props)
+    }
+
     static onDrop(files) {
-        console.log('Received files: ', files);
+        console.log('Received files: ', files)
+
+    }
+
+    getData() {
+        return [
+            {
+                lat: 47.0525,
+                long: 4.3837,
+                count: 2
+            },
+            {
+                lat: 47.1525,
+                long: 4.5837,
+                count: 8
+            }
+        ]
     }
 
     render() {
